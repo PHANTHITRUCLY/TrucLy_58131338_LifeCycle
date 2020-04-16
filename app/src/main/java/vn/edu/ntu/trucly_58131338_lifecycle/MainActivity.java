60cd0e900d657227eb4531de59c4a721f3f56a46
Phan Thị Trúc Ly <46109438+PHANTHITRUCLY@users.noreply.github.com>
2020-04-16 16:54:10 +0700

    v2.0

--- app/src/main/java/vn/edu/ntu/trucly_58131338_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/trucly_58131338_lifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package vn.edu.ntu.trucly_58131338_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle","onCreate called");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle","onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle","onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy called");
     }
 }
