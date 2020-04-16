e3735e36f811b66ee59c0fb2ce4a8904fcd95c9a
Phan Thị Trúc Ly <46109438+PHANTHITRUCLY@users.noreply.github.com>
2020-04-16 18:18:04 +0700

    v3.0

--- app/src/main/java/vn/edu/ntu/trucly_58131338_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/trucly_58131338_lifecycle/MainActivity.java
@@ -2,8 +2,10 @@ package vn.edu.ntu.trucly_58131338_lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,41 +14,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle","onCreate called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onCreate called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("LifeCycle","onStart called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onStart called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d("LifeCycle","onRestart called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onRestart called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("LifeCycle","onResume called");
+        Context context = getApplicationContext();
+        CharSequence text = "onResume called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d("LifeCycle","onPause called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onPause called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d("LifeCycle","onStop called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onStop called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d("LifeCycle","onDestroy called");
+
+        Context context = getApplicationContext();
+        CharSequence text = "onDestroy called!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 }
